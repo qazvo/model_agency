f2daf5e7419d520571697b08bf79a7f391f8cad3
qazvo <qazvo@bk.ru>
2023-09-30 11:34:35 +0300

    Update db.sql

--- SQL/db.sql
+++ SQL/db.sql
@@ -13,29 +13,29 @@ CREATE TABLE IF NOT EXISTS countries (
 CREATE TABLE IF NOT EXISTS customer_organizations (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(30) NOT NULL,
-    country_id INTEGER REFERENCES countries(id),
+    country_id INTEGER REFERENCES countries(id) ON DELETE CASCADE,
     contact_details VARCHAR(30) NOT NULL,
     inn INT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS events (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(30) NOT NULL,
-    country_id INTEGER REFERENCES countries(id),
-  	customer_organizations_id INTEGER REFERENCES customer_organizations(id),
+    country_id INTEGER REFERENCES countries(id) ON DELETE CASCADE,
+  	customer_organizations_id INTEGER REFERENCES customer_organizations(id) ON DELETE CASCADE,
     date  DATE NOT NULL,
     time TIME NOT NULL
 );
 CREATE TABLE IF NOT EXISTS contracts(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    event_id INTEGER REFERENCES events(id),
-    model_id INTEGER REFERENCES models(id),
+    event_id INTEGER REFERENCES events(id) ON DELETE CASCADE,
+    model_id INTEGER REFERENCES models(id) ON DELETE CASCADE,
     payment DECIMAL(15,2)
 );
 CREATE TABLE IF NOT EXISTS accounts_OC(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     login VARCHAR(20) NOT NULL,
     password VARCHAR(30) NOT NULL,
-    customer_organizations_id INTEGER REFERENCES customer_organizations(id)
+    customer_organizations_id INTEGER REFERENCES customer_organizations(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS administrators(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -47,5 +47,5 @@ CREATE TABLE IF NOT EXISTS accounts_administrator(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     login VARCHAR(20) NOT NULL,
     password VARCHAR(30) NOT NULL,
-    administrators INTEGER REFERENCES administrators(id)
+    administrators INTEGER REFERENCES administrators(id) ON DELETE CASCADE
 );
