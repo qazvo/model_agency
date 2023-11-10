f157fa0080d82bc6fb90421abd7bcff206fc845a
qazvo <qazvo@bk.ru>
2023-11-11 00:06:00 +0300

    1

--- source/server/start_server.py
+++ source/server/start_server.py
@@ -1,12 +1,19 @@
 from server.database.db_manager import db_manager
 import server.settings as settings, uvicorn
 from fastapi import FastAPI
-from server.routers import administrators
+from server.routers import administrators, contracts, countries, customer_organizations, events, models, roles, users
 from fastapi.responses import RedirectResponse
 
 app = FastAPI(title='model_agency')
 
 app.include_router(administrators)
+app.include_router(contracts)
+app.include_router(countries)
+app.include_router(customer_organizations)
+app.include_router(events)
+app.include_router(models)
+app.include_router(roles)
+app.include_router(users)
 
 @app.router.get('/')
 def start_page() -> RedirectResponse:
